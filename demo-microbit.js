c82b6ce92be32134b39c1a9aa1d4f0c9e753d78f
Daniel Jackson <email@danieljackson.co.uk>
2022-03-17 17:39:24 +0000

    Information about pairing to a micro:bit.

--- demo-microbit.js
+++ demo-microbit.js
@@ -1,9 +1,15 @@
 // Micro:bit UART peripheral demo
 //
 // At https://makecode.microbit.org/, in the Blocks column select "Advanced", "+ Extensions", 
-// search for `bluetooth`, select "bluetooth (Bluetooth services)", click "Remove extension(s) and add bluetooth", 
+// search for "bluetooth", select "bluetooth (Bluetooth services)", click "Remove extension(s) and add bluetooth", 
 // edit the blocks as below (you can paste this code in JavaScript view, and then return to Blocks view),
 // then "Download" the code to program your micro:bit.
+//
+// Important: You must first pair your computer with the micro:bit by holding down the micro:bit's _A_ + _B_ buttons 
+// while pressing the _Reset_ button on the back, then pair your computer.
+// For example, "bluetoothctl", "scan on", wait to see your device's address, "scan off", "pair A1:B2:C3:D4:E5:F6"
+// (substituting "A1:B2:C3:D4:E5:F6" with your device's address), wait to see "Pairing successful", "exit".
+//
 
 bluetooth.onBluetoothConnected(function () {
     basic.showIcon(IconNames.Yes)
